aaa5882d609365026200a2db8b4a310d6e1ce9da
Tianying Cui <cuitianying@gmail.com>
2016-05-16 19:18:09 +1000

    add new message type

--- DAStrategyGame/Transactions/Transactions.py
+++ DAStrategyGame/Transactions/Transactions.py
@@ -1,1 +1,27 @@
 #Transactions
+import message
+import time
+
+UUID = int(round(time.time() * 1000))
+MSG = message.Message(UUID)
+
+class Transaction:
+    def __init__(self, host, node):
+        self.__host = host
+        self.__node = node
+
+
+    def start_transaction(self):
+        message
+        self.__node.send_message(self, self.__host, MSG.)
+
+    def end_transaction(self):
+
+
+    def buy_resource(self):
+
+
+    def sell_resource(self):
+
+
+
