f2b5bfa85f86e889cb137af05c30c6e1f05f6355
Tianying Cui <cuitianying@gmail.com>
2016-05-16 21:21:00 +1000

    add transaction logic

--- DAStrategyGame/Transactions/Transactions.py
+++ DAStrategyGame/Transactions/Transactions.py
@@ -6,10 +6,16 @@ UUID = int(round(time.time() * 1000))
 
 
 class Transaction:
-    def __init__(self, host, node):
+    __food = 0
+    __wood = 0
+    __mineral = 0
+    __leather = 0
+    __money = 0
+    def __init__(self, host, msg, node, user):
         self.__host = host
         self.__node = node
-        self.__MSG = message.Message(UUID)
+        self.__MSG = msg
+        self.__user = user
 
 
     def start_transaction(self):
@@ -20,9 +26,29 @@ class Transaction:
 
     def buy_resource(self, resource, quantity):
         self.__node.send_message(self, self.__host, self.__MSG.buyResource(self, resource, quantity))
+        self.__user.add_resources(resource, quantity)
+        self.__user.consume_money(resource.getPrice() * quantity)
 
     def sell_resource(self, resource, quantity):
         self.__node.send_message(self, self.__host, self.__MSG.sellResource(self, resource, quantity))
+        self.__user.consume_resources(resource, quantity)
+        self.__user.add_money(resource.getPrice() * quantity)
+
+    def set_transaction_status(self, user):
+        self.__food = user.get_food()
+        self.__wood = user.get_wood()
+        self.__mineral = user.get_mineral()
+        self.__leather = user.get_leather()
+        self.__money = user.get_money()
+
+    def recover_transaction_status(self, user):
+        self.__user.set_resources(self.__food, self.__wood, self.__mineral, self.__leather, self.__money)
+        return self.__user
+
+
+
+
+
 
 
 
--- DAStrategyGame/User/User.py
+++ DAStrategyGame/User/User.py
@@ -8,11 +8,21 @@ class User:
         self.__leather = 1000
         self.__money = 10000
 
+    def set_resources(self, food, wood, mineral, leather, money):
+        self.__food = food
+        self.__wood = wood
+        self.__mineral = mineral
+        self.__leather = leather
+        self.__money = money
+
     def add_food(self, quantity):
         self.__food += quantity
 
     def comsume_food(self, quantity):
         self.__food -= quantity
+
+    def get_food(self):
+        return self.__food
 
     def add_wood(self, quantity):
         self.__wood += quantity
@@ -20,11 +30,17 @@ class User:
     def comsume_wood(self, quantity):
         self.__wood -= quantity
 
+    def get_wood(self):
+        return self.__wood
+
     def add_mineral(self, quantity):
         self.__mineral += quantity
 
     def comsume_mineral(self, quantity):
         self.__mineral -= quantity
+
+    def get_mineral(self):
+        return self.__mineral
 
     def add_leather(self, quantity):
         self.__leather += quantity
@@ -32,11 +48,42 @@ class User:
     def comsume_leather(self, quantity):
         self.__leather -= quantity
 
+    def get_leather(self):
+        return self.__leather
+
     def add_money(self, quantity):
         self.__money += quantity
 
     def comsume_money(self, quantity):
         self.__money -= quantity
+
+    def get_money(self):
+        return self.__money
+
+    def add_resources(self, resource, quantity):
+        if resource == 'food':
+            self.add_food(quantity)
+        elif resource == 'wood':
+            self.add_wood(quantity)
+        elif resource == 'mineral':
+            self.add_mineral(quantity)
+        elif resource == 'leather':
+            self.add_leather(quantity)
+        else:
+            print 'Wrong resource name!'
+            return
+
+    def consume_resources(self, resource, quantity):
+        if resource == 'food':
+            self.consume_food(quantity)
+        elif resource == 'wood':
+            self.consume_wood(quantity)
+        elif resource == 'mineral':
+            self.consume_mineral(quantity)
+        elif resource == 'leather':
+            self.consume_leather(quantity)
+        else:
+            return
 
     def show_resources(self):
         print 'Food: %d' % self.__food
