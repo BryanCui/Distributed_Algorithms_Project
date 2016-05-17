c992846a7de411ed0b08ab22e5697662da869560
Xiangyu Zhou <exloong@gmail.com>
2016-05-18 02:37:45 +1000

    add list select callbacks

--- DAStrategyGame/GUI/MainScreen.py
+++ DAStrategyGame/GUI/MainScreen.py
@@ -2,29 +2,30 @@
 # encoding: utf-8
 
 import npyscreen
-import types
+from PeopleList import PeopleList
 
 class MainScreen(npyscreen.Form):
 
     def __init__(self, name):
         npyscreen.Form.__init__(self, name="Tradercraft " + name)
 
-        self.add_widget(npyscreen.FixedText
-            , value="You have:"
-            , editable=False)
-
         self.inventoryText = self.add_widget(npyscreen.MultiLineEdit
             , value=self.getPlayerIventory()
+            , color='STANDOUT'
             , max_height=3
             , editable=False)
 
         self.add_widget(npyscreen.FixedText
-            , value=""
+            , relx=50
+            , rely=2
+            , value="7 People around you"
+            , color='IMPORTANT'
             , editable=False)
 
-        self.peopleSelection = self.add(npyscreen.TitleSelectOne
+        self.peopleSelection = self.add(PeopleList
+            , relx=50
+            , rely=3
             , max_height=5
-            , name="People around you:"
             , values = [
                 "People 1"
                 ,"People 2"
@@ -36,16 +37,14 @@ class MainScreen(npyscreen.Form):
             ]
             , scroll_exit=True)
 
-        self.peopleSelection.actionHighlighted = types.MethodType(self.action, self.peopleSelection)
+        self.refreshBtn = self.add_widget(npyscreen.Button
+            , relx=49
+            , rely=9
+            , name="Look around")
 
     # get the player's inventory in formatted string
     def getPlayerIventory(self):
         return '''
-         Gold: 1000     Iron: 10
-         Wood: 50       Food: 0
-        '''
-
-    def action(self, act_on_this, key_press):
-        popup = npyscreen.Popup(name="I am clicked")
-        popup.edit()
-
+Gold: 1000     Iron: 10
+Wood: 50       Food: 0
+'''
